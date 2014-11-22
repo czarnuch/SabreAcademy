e28fc363743c2a34bab1bb6076548dfcba1a54ee
Adam Zajac <corny03@gmail.com>
2014-11-22 10:01:26 +0100

    Stworzenie Funkcji Arytmetych

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 //maincpp
+#include "multiple.h"
+#include "add.h"
+#include<iostream>
 int main()
 {
+	std::cout << "->   wynik mnożenia: " << multiple(2,2) << "\n->   wynik dodawania: " <<  add(2,3) << std::endl;
+	std::cin.get();
+	return 0;
 	
 }
---
+++ multiple.h
@@ -0,0 +1,7 @@
+#ifndef MULTIPLE_H
+#define MULTIPLE_H
+int multiple(int a , int b)
+{
+	return a*b;
+}
+#endif
